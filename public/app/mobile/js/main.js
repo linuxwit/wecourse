1be5331d8843d97442e8b6332675e3d35b7defd4
linuxwit <linuxwit@gmail.com>
2015-02-11 23:19:04 +0800

    anim

---
+++ public/app/mobile/js/main.js
@@ -0,0 +1,21 @@
+$('.wp-inner').fullpage({
+change: function (e) {
+console.log('change', e.next, e.cur);
+$('.indicator li').removeClass('cur').eq(e.next).addClass('cur');
+   alert(e.cur);
+    $('.page'+(e.cur+2)).find('div.box').each(function(i){
+                        var anim=$(this).attr('data-anim');
+                        console.log(anim);
+                        $(this).removeClass(anim);
+                        setTimeout(function(box){
+                            box.addClass(anim);
+                        },300,$(this));
+                })
+},
+beforeChange: function (e) {
+console.log('beforeChange', e.next, e.cur);
+},
+afterChange: function (e) {
+console.log('afterChange', e.next, e.cur);
+}
+});
--- resources/views/mobile/welcome.blade.php
+++ resources/views/mobile/welcome.blade.php
@@ -1,63 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>Document</title>
-    <link rel="stylesheet" href="app/mobile/css/main.css">
-    <style>
-        .indicator{
-            position: fixed;
-            right: 10px;
-            top: 50%;
-            margin-top: -40px;
-        }
-        .indicator li{
-            margin-top: 10px;
-            width: 10px;
-            height: 10px;
-            border-radius: 100px;
-            background: rgba(0,0,0,0.5);
-            text-indent: -9999px;
-            list-style: none;
-        }
-        .indicator li.cur{
-            background: rgba(255,255,255,0.5);
-        }
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title>Document</title>
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+        <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
+        <!--<![endif]-->
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+        <link rel="stylesheet" href="app/mobile/css/main.css">
+        <link rel="stylesheet" href="app/mobile/css/all-animation.css">
+    </head>
+    <style type="text/css">
+    .box{
+    margin: 10px auto;
+    width: 30%;
+    height: 50px;
+    border: 1px solid #FFF;
+    }
     </style>
-</head>
-<body>
-    <div class="wp">
-        <div class="wp-inner">
-            <div class="page page1">1</div>
-            <div class="page page2">2</div>
-            <div class="page page3">3</div>
-            <div class="page page4">4</div>
+    <body>
+        <div class="wp">
+            <div class="wp-inner">
+                <div class="page page1">
+                    <div class="box" data-anim="four-rock">
+                        1
+                    </div>
+                </div>
+                <div class="page page2">2</div>
+                <div class="page page3">3</div>
+                <div class="page page4">4</div>
+            </div>
+            <ul class="indicator">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
         </div>
-        <ul class="indicator">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
-    </div>
-    <span class="start"><b></b></span>
-    <script src="app/mobile/js/zepto.min.js"></script>
-    <script src="app/mobile/js/zepto.fullpage.js"></script>
-    <script>
-        $('.wp-inner').fullpage({
-            change: function (e) {
-                console.log('change', e.next, e.cur);
-                $('.indicator li').removeClass('cur').eq(e.next).addClass('cur');
-            },
-            beforeChange: function (e) {
-                console.log('beforeChange', e.next, e.cur);
-            },
-            afterChange: function (e) {
-                console.log('afterChange', e.next, e.cur);
-            }
-        });
-    </script>
-</body>
+        <span class="start"><b></b></span>
+        <script src="app/mobile/js/zepto.min.js"></script>
+        <script src="app/mobile/js/zepto.fullpage.js"></script>
+        <script src="app/mobile/js/main.js"></script>
+    </body>
 </html>
